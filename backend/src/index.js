322d2dd15cab48f651e2492e13b6b99bfd41cc9e
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-09-20 15:38:21 +0900

    save

--- backend/src/index.js
+++ backend/src/index.js
@@ -65,15 +65,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	const frontendPath = path.join(__dirname, "../frontend/build"); // <-- adjust to your build folder
-	app.use(express.static(frontendPath));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(frontendPath, "index.html"));
+		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 	});
-	
 }
-
 
 server.listen(PORT, () => {
 	console.log("server is running on PORT:" + PORT);
