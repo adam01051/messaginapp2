4c4626f57ac3eabf04913de6fc00735c0cdf2238
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-09-20 15:43:28 +0900

    try

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
 	baseURL:
 		import.meta.env.MODE === "development"
 			? "http://localhost:5001/api"
-			: "https://app1-zaix.onrender.com/api", 
+			: "https://threerd-messagin-application.onrender.com",
 	withCredentials: true,
 });
