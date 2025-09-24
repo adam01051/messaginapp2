112ab1c8601b80afa7de5045f3a498d7614454ef
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-09-24 17:56:17 +0900

    auth

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,10 +7,9 @@ import { io } from "socket.io-client";
  
 
 const BASE_URL =
-	import.meta.env.MODE === "development"
-		? "http://localhost:5001"
-		: "https://app1-zaix.onrender.com";
+	import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 	
+
 export const useAuthStore = create((set, get) => ({
 	authUser: null,
 	isSigningUp: false,
