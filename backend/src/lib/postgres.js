1752135944e663452d1175a9419029dbaf39d593
loki0105 <63663677+loki0105@users.noreply.github.com>
2025-09-26 18:25:34 +0900

    just  posgres  the  ui did not work out

--- backend/src/lib/postgres.js
+++ backend/src/lib/postgres.js
@@ -4,10 +4,22 @@ config();
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: { rejectUnauthorized: false }, // required for Render
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+	isProduction
+		? {
+				connectionString: process.env.DATABASE_URL,
+				ssl: { rejectUnauthorized: false }, // Render / production
+		  }
+		: {
+				user: process.env.PG_NAME || "postgres",
+				password: process.env.PG_PASSWORD || "password",
+				host: process.env.PG_HOST || "localhost",
+				port: process.env.PG_PORT || 5432,
+				database: process.env.PG_DATABASE || "my_local_db",
+		  }
+);
 
 // Test the connection
 pool
@@ -21,33 +33,3 @@ pool
 	});
 
 export default pool;
-
-// -- Users table
-// CREATE TABLE users (
-//     id SERIAL PRIMARY KEY,
-//     name TEXT NOT NULL,
-//     username TEXT UNIQUE NOT NULL,
-//     password_ TEXT NOT NULL,
-//     profileImage TEXT,
-//     email TEXT UNIQUE,
-//     number TEXT
-// );
-
-// -- Contacts table
-// CREATE TABLE contacts (
-//     id SERIAL PRIMARY KEY,
-//     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-//     contact_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-//     UNIQUE (user_id, contact_id)
-// );
-
-//create user_contacts
-
-// -- Messages table
-// CREATE TABLE messages (
-//     id SERIAL PRIMARY KEY,
-//     sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-//     receiver_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-//     content TEXT NOT NULL,
-//     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-// );
